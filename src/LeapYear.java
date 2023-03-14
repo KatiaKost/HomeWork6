2878fa6faeab70897725c040dfe29bd6af10322b
KatiaKost <katya-donya@mail.ru>
2023-03-14 22:02:36 +1000

    HomeWork done

---
+++ src/LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = input.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " - високосный год.");
+        } else if (year % 400 == 0){
+            System.out.println(year + " - високосный год.");
+        } else {
+            System.out.println(year + "- не високосный год.");
+        }
+    }
+}
